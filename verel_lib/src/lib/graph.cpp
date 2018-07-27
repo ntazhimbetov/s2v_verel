b25dc44794bf879875a44fcb8a6a26e2dc7b0304
Nurbek <nurbek@stanford.edu>
2018-07-27 16:11:54 -0700

    Add files via upload

---
+++ verel_lib/src/lib/graph.cpp
@@ -0,0 +1,58 @@
+#include "graph.h"
+#include <cassert>
+#include <iostream>
+#include <random>
+
+Graph::Graph() : num_nodes(0), num_edges(0)
+{
+  adj_list.clear();
+  edge_list.clear();
+}
+
+Graph::Graph(const int _num_nodes, const int _num_edges, const int* edges_from, const int* edges_to)
+  : num_nodes(_num_nodes), num_edges(_num_edges)
+{
+  adj_list.resize(num_nodes);
+  edge_list.reserve(2*(num_edges-num_nodes));
+  for (int i = 0; i < num_nodes; ++i)
+    adj_list[i].clear();
+
+  for (int i = 0; i < num_edges; ++i)
+  {
+    int x = edges_from[i], y = edges_to[i];
+    if (x != y) {
+      adj_list[x].push_back(y);
+      adj_list[y].push_back(x);
+      edge_list.push_back(std::make_pair(edges_from[i], edges_to[i]));
+      edge_list.push_back(std::make_pair(edges_to[i], edges_from[i]));
+    }
+  }
+}
+
+GSet::GSet()
+{
+  graph_pool.clear();
+}
+
+void GSet::InsertGraph(int gid, std::shared_ptr<Graph> graph)
+{
+  assert(graph_pool.count(gid) == 0);
+
+  graph_pool[gid] = graph;
+}
+
+std::shared_ptr<Graph> GSet::Get(int gid)
+{
+  assert(graph_pool.count(gid));
+  return graph_pool[gid];
+}
+
+std::shared_ptr<Graph> GSet::Sample()
+{
+  assert(graph_pool.size());
+  int gid = rand() % graph_pool.size();
+  assert(graph_pool[gid]);
+  return graph_pool[gid];
+}
+
+GSet GSetTrain, GSetTest;
---
+++ verel_lib/src/lib/verel_env.cpp
@@ -0,0 +1,113 @@
+#include "verel_env.h"
+#include "graph.h"
+#include <cassert>
+#include <random>
+#include <algorithm>
+#include <iostream>
+
+VerelEnv::VerelEnv(double _norm) : IEnv(_norm)
+{
+}
+
+void VerelEnv::s0(std::shared_ptr<Graph> _g)
+{
+  graph = _g;                // pointer to the graph object
+  eliminated_set.clear();    // eliminated set
+  numEliminatedNodes = 0;    // num of eliminated nodes
+  action_list.clear();       // action list
+
+  state_seq.clear();         // sequence of states
+  act_seq.clear();           // sequence of actions
+  reward_seq.clear();        // sequence of rewards
+  sum_rewards.clear();       // sum of rewards
+}
+
+// next step action
+double VerelEnv::step(int a)
+{
+  assert(graph);
+  // checking whether the node a is in the list of eliminated nodes
+  assert(eliminated_set.count(a) == 0);
+  assert(a < graph->num_nodes);
+  
+  state_seq.push_back(action_list);
+  act_seq.push_back(a);
+  
+  // adding the node to the list of eliminated nodes
+  action_list.push_back(a);
+  eliminated_set.insert(a);
+  numEliminatedNodes += 1;
+  // VERTEX ELIMINATION ACTION
+  double r_t = eliminate_node(a);
+  
+  reward_seq.push_back(r_t);
+  sum_rewards.push_back(r_t);
+
+  return r_t;
+}
+
+// Pick a random node to eliminate
+int VerelEnv::randomAction()
+{
+  assert(graph);
+  avail_list.clear();
+
+  for (int i = 0; i < graph->num_nodes; ++i)
+    if (eliminated_set.count(i) == 0)
+      avail_list.push_back(i);
+
+  assert(avail_list.size());
+  int idx = rand() % avail_list.size();
+  return avail_list[idx];
+}
+
+// Terminatation state when all the vertices are eliminated
+bool VerelEnv::isTerminal()
+{
+  assert(graph);
+  return graph->num_nodes == numEliminatedNodes;
+}
+
+// Define reward value
+double VerelEnv::eliminate_node(int new_node) {
+  
+  int num_new_edges = 0;
+
+  while(graph->adj_list[new_node].empty() == false) {
+    int neigh1_val = graph->adj_list[new_node].back();
+    
+    // deleting from the dajacency list from both copies
+    graph->adj_list[new_node].pop_back();
+    auto it = std::find(graph->adj_list[neigh1_val].begin(),
+		    graph->adj_list[neigh1_val].end(),
+		    new_node);
+    graph->adj_list[neigh1_val].erase(it);
+
+    // extra
+    for (auto i = 0; i < graph->edge_list.size(); ++i) {
+      if ((std::get<0>(graph->edge_list[i]) == new_node && std::get<1>(graph->edge_list[i]) == neigh1_val) 
+	  || (std::get<0>(graph->edge_list[i]) == neigh1_val && std::get<1>(graph->edge_list[i]) == new_node)) {
+	graph->edge_list.erase(graph->edge_list.begin() + i);
+	i--;
+      }
+    }
+
+    // updating the adjacency list and adding new edges
+    for (auto& neigh2_val : graph->adj_list[new_node]) {
+      if (std::find(graph->adj_list[neigh2_val].begin(),
+		    graph->adj_list[neigh2_val].end(),
+		    neigh1_val) == graph->adj_list[neigh2_val].end()) {
+	num_new_edges += 1;
+	graph->adj_list[neigh2_val].push_back(neigh1_val);
+	graph->adj_list[neigh1_val].push_back(neigh2_val);
+
+	// extra
+	graph->edge_list.push_back(std::make_pair(neigh1_val, neigh2_val));
+	graph->edge_list.push_back(std::make_pair(neigh2_val, neigh1_val));
+      }
+    }
+  }
+
+  //return ((-1.0) * num_new_edges) / norm;
+  return num_new_edges;
+}
